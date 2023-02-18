75ced85935e3db6a15c80e675e7490eaeaf85d3b
abdullahkaraboga <abdullahkaraboga@icloud.com>
2023-02-18 18:45:28 +0300

    Initial Commit

---
+++ GridVideoView/GridVideoViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridVideoViewApp.swift
+//  GridVideoView
+//
+//  Created by Abdullah Karaboğa on 18.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct GridVideoViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
